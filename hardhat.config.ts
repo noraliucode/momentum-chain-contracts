6de3d3a72b092be3d3b9dabf72d723fd3186b75a
Nora Liu <noraliu0830@gmail.com>
2022-08-14 19:51:55 +0800

    fix: update config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,42 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+require("@nomiclabs/hardhat-waffle")
+require("hardhat-gas-reporter")
+require("@nomiclabs/hardhat-etherscan")
+require("dotenv").config()
+require("solidity-coverage")
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
 
-const config: HardhatUserConfig = {
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {},
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 5,
+    },
+    // localhost: {
+    //   url: "http://localhost:8545",
+    //   chainId: 31337,
+    // },
+  },
   solidity: "0.8.4",
-};
-
-export default config;
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  // gasReporter: {
+  //   enabled: true,
+  //   currency: "USD",
+  //   outputFile: "gas-report.txt",
+  //   noColors: true,
+  //   coinmarketcap: COINMARKETCAP_API_KEY,
+  // },
+}
